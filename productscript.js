001974d9d3d6cb7304d2a33fb4433613780890e6
Farhan Ali <farhanali0006@gmail.com>
2023-07-04 19:43:39 +0500

    done changes

--- productscript.js
+++ productscript.js
@@ -13,5 +13,35 @@ async function getSingleProduct(){
 
     var title = document.getElementById("singleTitle")
     title.innerHTML = product.title
+
+    var singleImg = document.getElementById("img")
+    singleImg.src = product.image
+
+
+    var desc = document.getElementById("description")
+    desc.innerHTML = product.description
+
+
+    var price = document.getElementById("singlePrice")
+    price.innerHTML = "$"+product.price
+
+var rating = document.getElementsByClassName("single-rating")[0]
+console.log("youuuu",rating)
+
+    const filledStars = Math.round(product.rating.rate);
+for (let starIndex = 0; starIndex < 5; starIndex++) {
+    var star = document.createElement("span")
+    
+    star.className = "fa fa-star"
+    star.classList.add("star-single")
+    if (starIndex<filledStars) {
+        star.classList.add("filled")
+        
+    }
+   
+    rating.appendChild(star)
+    
+    
+}
 }
 window.onload = getSingleProduct()
